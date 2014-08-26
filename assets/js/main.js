a79d65b64374a8d011644d3f53de8cac7e7401a5
Ian Noyes <ian@12southmusic.com>
2014-08-26 12:50:25 -0500

    div wrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ var Roots = {
         instagram_id: 179338873,
         thumb_width: 306,
         limit: 12,
-        wrap_tag: 'div class=col-sm-3', '/div'
+        wrap_tag: 'div class=col-sm-3'
       });
     }
   },
