e32f69111393154e2cf3f3851bdd0b7906075851
Ian Noyes <ian@12southmusic.com>
2014-08-26 16:27:11 -0500

    wppageNavi

--- lib/custom.php
+++ lib/custom.php
@@ -1,4 +1,21 @@
-<?php 
+<?php
+
+// Get the page number into wp_pagenavi
+function pagenavi_paged($q) {
+  $types = (array)$q->get('post_type');
+  if (in_array('test', $types)) {
+    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+    $q->set('paged', $paged);
+  }
+}
+add_action('pre_get_posts', 'pagenavi_paged');
+
+// Replace wpv-pagination shortcode
+function wpv_pagenavi($args, $content) {
+  global $WP_Views;
+  wp_pagenavi( array('query' => $WP_Views->post_query) );
+}
+add_shortcode('wpv-pagination', 'wpv_pagenavi');
 
 define( 'UPLOADS', ''.'assets' );
 
