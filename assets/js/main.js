d57d81003ca6673b546e71c7b55ba108ef12b6d7
Ian Noyes <ian@12southmusic.com>
2014-08-27 16:07:37 -0500

    debug time

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,7 @@ var Roots = {
   // All pages
   common: {
     init: function() {
+      console.log('main.js loaded');
       // JavaScript to be fired on all pages
       $('.vid-container').fitVids();
       //$.okvideo({ source: 'https://vimeo.com/103495412', volume: 0, adproof: true });
