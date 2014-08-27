c1380dd2df727065554b6bdd550e2d99b9ca3775
Ian Noyes <ian@12southmusic.com>
2014-08-27 10:05:23 -0500

    ajaxcontent

--- base.php
+++ base.php
@@ -18,7 +18,7 @@
   ?>-
 
   <div class="wrap container" role="document" >
-    <div class="content row" id='content'>
+    <div class="content row" id='ajaxcontent'>
       <main class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
