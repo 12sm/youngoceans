7b80f0ec6b275ba5a334ef53d7be7dc6fdcec46c
Ian Noyes <ian@12southmusic.com>
2014-08-27 15:30:55 -0500

    no id

--- base.php
+++ base.php
@@ -1,5 +1,5 @@
 <?php get_template_part('templates/head'); ?>
-<body id='bodytag' <?php body_class(); ?>>
+<body <?php body_class(); ?>>
 
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
