36e632c7bc91a56ff0b8691cedbe4f9da65e9dd2
Ian Noyes <ian@12southmusic.com>
2014-08-28 12:21:05 -0500

    new shawtkood

--- lib/custom.php
+++ lib/custom.php
@@ -2,20 +2,20 @@
 
 // Get the page number into wp_pagenavi
 function pagenavi_paged($q) {
-  $types = (array)$q->get('post_type');
-  if (in_array('discography', $types)) {
-    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
-    $q->set('paged', $paged);
-  }
+if (get_query_var('paged')) {
+$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+$q->set('paged', $paged);
+}
 }
 add_action('pre_get_posts', 'pagenavi_paged');
 
-// Replace wpv-pagination shortcode
+// Add a custom wp_pagenavi shortcode
 function wpv_pagenavi($args, $content) {
-  global $WP_Views;
-  wp_pagenavi( array('query' => $WP_Views->post_query) );
+global $WP_Views;
+wp_pagenavi( array('query' => $WP_Views->post_query) );
 }
-add_shortcode('wpv-pagination', 'wpv_pagenavi');
+add_shortcode('wpv-pagenavi', 'wpv_pagenavi');
+
 
 define( 'UPLOADS', ''.'assets' );
 
