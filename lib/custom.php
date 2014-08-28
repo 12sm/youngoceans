51fde020646011fa62552909b5357fcc1291f9db
Ian Noyes <ian@12southmusic.com>
2014-08-28 18:45:10 -0500

    next disco

--- lib/custom.php
+++ lib/custom.php
@@ -1,7 +1,7 @@
 <?php
 
-add_shortcode('wpv-next-disco', 'next_post');
-function next_post(){
+add_shortcode('wpv-next-disco', 'next_disco');
+function next_disco(){
 	$next_post = get_next_post();
 	if (!empty( $next_post )): ?>
   	<a href="<?php echo get_permalink( $next_post->ID ); ?>"><i class='fa fa-angle-right'></i></a>
