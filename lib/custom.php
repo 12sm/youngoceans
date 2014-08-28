25ce5f3ece53f9345656a9166cac51ede7b1dbeb
Ian Noyes <ian@12southmusic.com>
2014-08-28 14:52:55 -0500

    without shortcode

--- lib/custom.php
+++ lib/custom.php
@@ -1,14 +1,14 @@
 <?php
 
 
-add_shortcode('wpv-post-index', 'views_index');
+/*add_shortcode('wpv-post-index', 'views_index');
 function views_index() {
     global $WP_Views;
     static $i = 0;
     $i ++;
     $paged = $WP_Views->post_query->get('paged') - 1;
     return $WP_Views->post_query->get('posts_per_page') * $paged + $i;
-}
+}*/
 
 define( 'UPLOADS', ''.'assets' );
 
