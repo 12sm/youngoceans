d26508886aa5813ab5eea0aa0ccb934e9a3a94f8
Ian Noyes <ian@12southmusic.com>
2014-08-28 14:07:35 -0500

    shortcode

--- lib/custom.php
+++ lib/custom.php
@@ -16,6 +16,14 @@ wp_pagenavi( array('query' => $WP_Views->post_query) );
 }
 add_shortcode('wpv-pagenavi', 'wpv_pagenavi');
 
+add_shortcode('wpv-post-index', 'views_index');
+function views_index() {
+    global $WP_Views;
+    static $i = 0;
+    $i ++;
+    $paged = $WP_Views->post_query->get('paged') - 1;
+    return $WP_Views->post_query->get('posts_per_page') * $paged + $i;
+}
 
 define( 'UPLOADS', ''.'assets' );
 
