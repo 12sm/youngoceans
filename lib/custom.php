97b8a970412b785f4a941191816fa1a23e040373
Ian Noyes <ian@12southmusic.com>
2014-08-28 16:26:03 -0500

    try this

--- lib/custom.php
+++ lib/custom.php
@@ -7,7 +7,7 @@ function views_index() {
     static $i = 0;
     $i ++;
     $paged = $WP_Views->post_query->get('paged') - 1;
-    return $WP_Views->post_query->get('posts_per_page') * $paged + $i;
+    return $paged + $i;
 }
 
 define( 'UPLOADS', ''.'assets' );
