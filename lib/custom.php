5b74e004ff925562328fbcaa22c320271aee6d93
Ian Noyes <ian@12southmusic.com>
2014-08-28 14:31:04 -0500

    no more pagenavi

--- lib/custom.php
+++ lib/custom.php
@@ -1,20 +1,5 @@
 <?php
 
-// Get the page number into wp_pagenavi
-function pagenavi_paged($q) {
-if (get_query_var('paged')) {
-$paged = get_query_var('paged') ? get_query_var('paged') : 1;
-$q->set('paged', $paged);
-}
-}
-add_action('pre_get_posts', 'pagenavi_paged');
-
-// Add a custom wp_pagenavi shortcode
-function wpv_pagenavi($args, $content) {
-global $WP_Views;
-wp_pagenavi( array('query' => $WP_Views->post_query) );
-}
-add_shortcode('wpv-pagenavi', 'wpv_pagenavi');
 
 add_shortcode('wpv-post-index', 'views_index');
 function views_index() {
