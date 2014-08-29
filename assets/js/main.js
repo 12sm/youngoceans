4a0e65a975011ec96a8c4e9a744690a003897aa7
Ian Noyes <ian@12southmusic.com>
2014-08-29 14:10:12 -0500

    vid

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,7 @@ var Roots = {
       //
       console.log('discographies');
       debugger;
-      var vidBG = document.getElementById("#vidBG").innerHTML;
+      var vid = document.getElementById("#vidBG").innerHTML;
       function vidBG(url){
                   debugger;
         $.okvideo({ source: url, volume: 0, adproof: true });
