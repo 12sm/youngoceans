eb87cb3f0f710a579a735d57369cccd64bce3cb8
Ian Noyes <ian@12southmusic.com>
2014-09-02 11:39:33 -0500

    switch controls

--- lib/custom.php
+++ lib/custom.php
@@ -1,18 +1,18 @@
 <?php
 
-add_shortcode('wpv-prev-disco', 'next_disco');
+add_shortcode('wpv-prev-disco', 'prev_disco');
 function next_disco(){
 	$next_post = get_next_post();
 	if (!empty( $next_post )): ?>
-  	<a href="<?php echo get_permalink( $next_post->ID ); ?>" class='disco-prev'><i class='fa fa-angle-left'></i></a>
+  	<a href="<?php echo get_permalink( $next_post->ID ); ?>" class='disco-prev'><i class='fa fa-angle-right'></i></a>
 <?php endif;
 	}
 
-add_shortcode('wpv-next-disco', 'prev_disco');
+add_shortcode('wpv-next-disco', 'next_disco');
 function prev_disco(){
 	$prev_post = get_previous_post();
 	if (!empty( $prev_post )): ?>
-  	<a href="<?php echo get_permalink( $prev_post->ID ); ?>" class='disco-next'><i class='fa fa-angle-right'></i></a>
+  	<a href="<?php echo get_permalink( $prev_post->ID ); ?>" class='disco-next'><i class='fa fa-angle-left'></i></a>
 <?php endif;
 	}
 
