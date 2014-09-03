751b71b2f653d2c60968c79141d05d154203c6cd
Ian Noyes <ian@12southmusic.com>
2014-09-03 16:52:10 -0500

    hd:true

--- assets/js/bgvid.js
+++ assets/js/bgvid.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	$.okvideo({ source: 'https://vimeo.com/103495412', volume: 0, adproof: true });
+	$.okvideo({ source: 'https://vimeo.com/103495412', volume: 0, adproof: true, hd: true });
 });
