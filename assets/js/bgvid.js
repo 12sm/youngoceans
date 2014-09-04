b35c0b892ba59f9e6c6094c0231555d832d8371d
Ian Noyes <ian@12southmusic.com>
2014-09-04 11:20:52 -0500

    url plz

--- assets/js/bgvid.js
+++ assets/js/bgvid.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$.okvideo({  playlist: {
-                	list: 'PLbngbkcCcQnP5TFKxViUh-rUr7iQbOunt', // a YT playlist id
+                	list: 'https://www.youtube.com/playlist?list=PLbngbkcCcQnP5TFKxViUh-rUr7iQbOunt', // a YT playlist id
                 	suggestedQuality: 'hd720'
               		},
           		volume: 0,
