8ce2b99d28aedb3f9684f78aa0f4ef279f32fcbc
Ian Noyes <ian@12southmusic.com>
2014-09-04 10:58:03 -0500

    video

--- assets/js/bgvid.js
+++ assets/js/bgvid.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	$.okvideo({ id: 'PLbngbkcCcQnP5TFKxViUh-rUr7iQbOunt', volume: 0, adproof: true, hd: true });
+	$.okvideo({ video: 'PLbngbkcCcQnP5TFKxViUh-rUr7iQbOunt', volume: 0, adproof: true, hd: true });
 });
