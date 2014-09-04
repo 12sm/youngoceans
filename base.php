9e7eb6bef15cbcf2b9e11f5bf6ec96a75c7c91a2
Ian Noyes <ian@12southmusic.com>
2014-09-04 16:16:12 -0500

    add #background

--- base.php
+++ base.php
@@ -6,7 +6,7 @@
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
   <![endif]-->
-
+<div id="background"></div>
   <?php
     do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
