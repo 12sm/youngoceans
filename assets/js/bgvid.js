3e9c184fba943d74607ea640217be6acc80304c0
Ian Noyes <ian@12southmusic.com>
2014-09-04 12:08:53 -0500

    diff id

--- assets/js/bgvid.js
+++ assets/js/bgvid.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$.okvideo({  playlist: {
-                	list: 'LbngbkcCcQnP5TFKxViUh-rUr7iQbOunt', // a YT playlist id
+                	list: 'm4gQTYGHfRg&list=RDHCbsJTbljgDIs', // a YT playlist id
                 	suggestedQuality: 'hd720'
               		},
           		volume: 0,
