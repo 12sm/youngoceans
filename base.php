6201026faae4b82e1e15d9bd454966851053186d
Ian Noyes <ian@12southmusic.com>
2014-09-08 15:21:38 -0500

    overlay

--- base.php
+++ base.php
@@ -5,7 +5,9 @@
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
   <![endif]-->
-<div id="background"><div id="player" style="position: absolute; width:100%; height: 100%;"></div></div>
+  <div id='bg-overlay'>
+    <div id="background"><div id="player" style="position: absolute; width:100%; height: 100%;"></div></div>
+  </div>
   <?php
     do_action('get_header');
     // Use Bootstrap's navbar if enabled in config.php
