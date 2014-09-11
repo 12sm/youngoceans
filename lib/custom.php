3e77b0a8e6bee5cce7d46c8822daa4898b4b18ff
Ian Noyes <ian@12southmusic.com>
2014-09-11 10:39:24 -0500

    trim

--- lib/custom.php
+++ lib/custom.php
@@ -35,6 +35,14 @@ function parse_Id($atts){
   return "{$matches[0]}";
   }
 
+add_shortcode('trim', 'trim_shortcode');
+function trim_shortcode($atts, $content = '') {
+  $content = wpv_do_shortcode($content);
+  if (strlen($content) > 20) {
+    $content = substr($content, 0, 20) . '…';
+  }
+  return $content;
+}
 
 define( 'UPLOADS', ''.'assets' );
 
