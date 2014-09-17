3a977c7e0caa6e59dc7643178aaff08079fac73f
Ian Noyes <ian@12southmusic.com>
2014-09-17 18:24:43 -0500

    shit

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,7 +137,7 @@ resources: {
 
     }
   },
-  photography: {
+  gallery: {
     init: function(){
       function liquidWrap(){
         $('.insta-photo').addClass('col-sm-6 col-md-4 col-lg-3');
