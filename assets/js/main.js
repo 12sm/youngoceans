4885d1dd54ca83e1a629424ae96ce30b58b6ef15
Ian Noyes <ian@12southmusic.com>
2014-09-22 01:35:05 -0500

    stuff and shit

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,27 +26,20 @@ var Roots = {
   common: {
     init: function() {
 
+   soundManager.setup({
+  // where to find flash audio SWFs, as needed
+  url: '/assets/js/',
+       debugMode: true,
+          debugFlash: true,
+  onready: window.playBG();
+});
+
 
 
 $('#stop-player').click(function(){
   soundManager.togglePause('bgSound');
 });
-    /*  soundManager.setup({
-  // disable or enable debug output
-  debugMode: true,
-  // use HTML5 audio for MP3/MP4, if available
-  preferFlash: false,
-  useFlashBlock: true,
-  // path to directory containing SM2 SWF
-  url: '/assets/js',
-  // optional: enable MPEG-4/AAC support (requires flash 9)
-  flashVersion: 9
-  });
 
-      soundManager.onready(function() {
-    // soundManager.createSound() etc. may now be called
-    inlinePlayer = new InlinePlayer();
-  });*/
 
       $('.fancypdf').fancybox({
             'autoSize'      : false,
