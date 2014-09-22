1cb22b77f8ef3d38f1d2fe9d89966e6190f73974
Ian Noyes <ian@12southmusic.com>
2014-09-22 14:15:46 -0500

    Revert "this stuff"
    This reverts commit 041c945af8d6ce4cace59e43845affc0bb50f7bf.

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,7 +111,12 @@ var Roots = {
     init: function() {
       // JavaScript to be fired on the home page
 
- shopifyEmbed();
+ var ShopifyStoreConfig = {shop:"young-oceans.myshopify.com", collections:[25880931,26665103,26699619]};
+ (function() {
+ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+ s.src = "//widgets.shopifyapps.com/assets/shopifystore.js";
+ var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+ })();
     }
   },
 resources: {
