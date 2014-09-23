6680cd0c6699eb9e1bbbf1693c5bf6714447de82
Ian Noyes <ian@12southmusic.com>
2014-09-23 15:44:00 -0500

    new shop

--- templates/head.php
+++ templates/head.php
@@ -31,6 +31,15 @@ http://12southmusic.com/
     echo do_shortcode('[wpv-view name="BACKGROUND MUSIC"]');
   ?>
 
+<script type="text/javascript">
+  var ShopifyStoreConfig = {shop:"young-oceans.myshopify.com", collections:[26699619,25880931,26665103]};
+  (function() {
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+    s.src = "//widgets.shopifyapps.com/assets/shopifystore.js";
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+  })();
+</script>
+<noscript>Please enable javascript, or <a href="http://young-oceans.myshopify.com">click here</a> to visit my <a href="http://www.shopify.com/tour/ecommerce-website">ecommerce web site</a> powered by Shopify.</noscript>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <link rel="shortcut icon" href="<?php echo of_get_option('favicon_upload'); ?>" />
